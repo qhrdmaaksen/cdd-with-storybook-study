267f1d0265a3c15ac51fb249f54f448bfaa0c958
vitamin777 <rlaalsdn8@gmail.com>
2023-06-25 16:26:25 +0900

    feat: Button, ButtonGroup(card)
    - Button 및 ButtonGroup 컴포넌트 생성해 index.js 에서 import & export 로App.jsx 에서 적용 시킴

--- src/components/MyButton/MyButton.jsx
+++ src/components/MyButton/MyButton.jsx
@@ -4,46 +4,52 @@ import './MyButton.css';
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={backgroundColor && { backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
+export const Button = ({primary, backgroundColor, radius, size, label, ...props}) => {
+    const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+    return (
+        <button
+            type="button"
+            className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+            style={
+                (backgroundColor && {backgroundColor},
+                radius ? {borderRadius: radius + 'em'} : {borderRadius: 0})
+            }
+            {...props} // js 문법에서 나머지 매개 변수라고 함 <Button a,b,c 외 d, e /> (REST parameter)
+        >
+            {label}
+        </button>
+    );
 };
 
 Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * MyButton contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
+    /**
+     * Is this the principal call to action on the page?
+     */
+    primary: PropTypes.bool,
+    /**
+     * What background color to use
+     */
+    backgroundColor: PropTypes.string,
+    /**
+     * How large should the button be?
+     */
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    /**
+     * MyButton contents
+     */
+    label: PropTypes.string.isRequired,
+    /**
+     * Optional click handler
+     */
+    onClick: PropTypes.func,
+
+    radius: PropTypes.string,
 };
 
 Button.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  size: 'medium',
-  onClick: undefined,
+    backgroundColor: null,
+    primary: false,
+    size: 'medium',
+    onClick: undefined,
+    radius: null,
 };
